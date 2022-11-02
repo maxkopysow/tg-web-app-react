768c13ec5b15a4429cf77a5b6d1ad971e17bcee2
maxkopysow <maxkopysow@gmail.com>
2022-11-02 16:39:21 +0300

    adding Form

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useTelegram } from '../../hooks/useTelegram';
+import './Form';
+
+const Form = (props) => {
+   const {user} = useTelegram();
+    return (
+       <div className={'form'}>
+            <h3>Введите ваши данные</h3>
+            <input 
+               className={'input'} 
+               type="text" 
+               placeholder={'ФИО'}
+            />
+            <input 
+               className={'input'} 
+               type="text" 
+               placeholder={'Наименование компании'}
+            />
+            <input 
+               className={'input'} 
+               type="text" 
+               placeholder={'ИНН компании'}
+            />
+            <input 
+               className={'input'} 
+               type="text" 
+               placeholder={'e-mail'}
+            />
+            <input 
+               className={'input'} 
+               type="text" 
+               placeholder={'номер телефона'}
+            />
+       </div>
+    );
+};
+export default Form;
