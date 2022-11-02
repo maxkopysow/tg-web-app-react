2d48cb938b7e1559f9ab245939f690944e543cad
maxkopysow <maxkopysow@gmail.com>
2022-11-02 13:50:09 +0300

    init

--- src/App.js
+++ src/App.js
@@ -5,14 +5,11 @@ function App() {
   useEffect(() => {
     tg.ready();
   },[])
-  const onClose = () =>{
-    tg.close();
-  }
+
   
   return (
     <div className="App">
       work
-      <button onClick={onClose}>Закрыть</button>
     </div>
   );
 }
