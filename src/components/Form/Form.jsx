aa3de7b280dc5da461921d3d82ead1743245d8f8
maxkopysow <maxkopysow@gmail.com>
2022-11-04 00:02:17 +0300

    test

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -72,7 +72,7 @@ const Form = () => {
    }
    return (
        <div className={'form'}>
-            <p>{tg.initDataUnsafe.chat}</p>
+            <p>{tg.initDataUnsafe.chat.id}</p>
             <h3>Введите ваши данные</h3>
             <h3>query {queryId}</h3>
             <h3>chat {chatId}</h3>
