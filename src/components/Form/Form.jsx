83768b805e40703b3fcc08fa3556efc4e1a4f5dc
maxkopysow <maxkopysow@gmail.com>
2022-11-03 23:41:10 +0300

    test

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -23,7 +23,7 @@ const Form = () => {
       headers:{"Content-Type":"application/json"},
       method: 'POST', 
       mode: 'cors', 
-      body: tg.initData // body data type must match "Content-Type" header
+      body: tg.initDataUnsafe // body data type must match "Content-Type" header
     })
    
    
