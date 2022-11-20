3377c5ed28710da9b605167126bd135aef8e6196
maxkopysow <maxkopysow@gmail.com>
2022-11-20 21:00:51 +0300

    test

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -31,7 +31,7 @@ const Form = () => {
          "phoneNumber":phoneNumber.value,
       }
      
-      fetch('http://localhost:3000/web-data', {  // Enter your IP address here
+      fetch('https://158.160.14.193:3000/web-data', {  // Enter your IP address here
          headers:{"Content-Type":"application/json"},
          method: 'POST', 
          body: JSON.stringify(data) // body data type must match "Content-Type" header
